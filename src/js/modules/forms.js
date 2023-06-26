c18730172526226bcca3aee58c1e533da11f2ce3
Ivan <igorlov18@gmail.com>
2023-06-26 17:44:04 +0200

    first commit

---
+++ src/js/modules/forms.js
@@ -0,0 +1,65 @@
+import checkNumInputs from "./checkNumInputs";
+
+const forms = (state) => {
+  const form = document.querySelectorAll("form"),
+    inputs = document.querySelectorAll("input");
+
+  checkNumInputs('input[name="user_phone"]');
+
+  const message = {
+    loading: "Загрузка...",
+    success: "Спасибо! Скоро с Вами свяжемся",
+    failure: "Ошибка",
+  };
+
+  const postData = async (url, data) => {
+    document.querySelector(".status").textContent = message.loading;
+    let res = await fetch(url, {
+      method: "POST",
+      body: data,
+    });
+
+    return await res.text();
+  };
+
+  function clearInputs() {
+    inputs.forEach((item) => {
+      item.value = "";
+    });
+  }
+
+  form.forEach((item) => {
+    item.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      item.appendChild(statusMessage);
+
+      const formData = new FormData(item);
+      if (item.getAttribute("data-calc") === "end") {
+        for (let key in state) {
+          formData.append(key, state[key]);
+        }
+      }
+
+      postData("assets/server.php", formData)
+        .then((res) => {
+          console.log(res);
+          statusMessage.textContent = message.success;
+        })
+        .catch((res) => {
+          console.log(res);
+          statusMessage.textContent = message.failure;
+        })
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
+        });
+    });
+  });
+};
+
+export default forms;
